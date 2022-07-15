d546abe2daa1c3dde5b77d30118e6c6be8f8e00b
Xdecent <100188867+Xdecent0@users.noreply.github.com>
2022-07-15 20:55:08 +0300

    completed task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.getElementById("ingredients");
+let listArray = [];
+
+for (let ingredient of ingredients) {
+  const ingredientsItem = document.createElement("li");
+  ingredientsItem.innerText = ingredient;
+  ingredientsItem.classList.add("item");
+  listArray.push(ingredientsItem);
+}
+ingredientsList.append(...listArray);
+console.log(ingredientsList);
