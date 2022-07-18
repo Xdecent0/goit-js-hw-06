12c7566ac183c0d729b445706e84aac98535d4aa
Xdecent <100188867+Xdecent0@users.noreply.github.com>
2022-07-18 16:09:57 +0300

    fixed 7 task

--- js/task-07.js
+++ js/task-07.js
@@ -2,6 +2,6 @@ const inputRange = document.getElementById("font-size-control");
 const text = document.getElementById("text");
 text.style.fontSize = `${inputRange.value}px`;
 
-inputRange.addEventListener("change", function () {
+inputRange.addEventListener("input", function () {
   text.style.fontSize = `${inputRange.value}px`;
 });
