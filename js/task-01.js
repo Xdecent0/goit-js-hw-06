606d74f315e93dab32b6a9368b44e4842def6940
Xdecent <100188867+Xdecent0@users.noreply.github.com>
2022-07-18 16:01:41 +0300

    first task

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,8 @@
 const categories = document.querySelectorAll(".item");
 console.log(`Number of categories: ${categories.length}`);
 categories.forEach((category) => {
-  console.log(`Category: ${category.children.item(0).textContent}`);
-  console.log(`Elements: ${category.children.item(1).children.length}`);
+  console.log(`Category: ${category.firstElementChild.textContent}`);
+  console.log(
+    `Elements: ${category.firstElementChild.nextElementSibling.children.length}`
+  );
 });
