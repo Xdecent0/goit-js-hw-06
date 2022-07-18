7d38b1f635531d059574829b7d42c3ff6cdb7411
Xdecent <100188867+Xdecent0@users.noreply.github.com>
2022-07-18 16:07:40 +0300

    fixed task 6

--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,9 @@ const symbolsCount = Number(textInput.dataset.length);
 textInput.addEventListener("blur", () => {
   if (textInput.value.length === symbolsCount) {
     textInput.classList.add("valid");
+    textInput.classList.remove("invalid");
   } else {
     textInput.classList.add("invalid");
+    textInput.classList.remove("valid");
   }
 });
